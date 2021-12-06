7cd4f6384eb54871820671cf318a8f463246b358
Nurul-GC <nuruldecarvalho@gmail.com>
2021-12-07 01:06:39 +0200

    update extra-src

--- git.sh
+++
@@ -1,7 +0,0 @@
-echo "# GCsortstrings" >> README.md
-git init
-git add ./*
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Nurul-GC/GCsortstrings.git
-git push -u origin main
